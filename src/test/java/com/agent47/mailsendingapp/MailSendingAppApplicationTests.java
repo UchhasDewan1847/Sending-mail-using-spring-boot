066d3ce6250156a035fefa5ab1f5ba9ab83b553f
Uchhas <uchhasmacap@gmail.com>
2023-12-03 17:54:22 +0600

    This is the way of sending mail using the JavaMailSender and MailProperties Library

---
+++ src/test/java/com/agent47/mailsendingapp/MailSendingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent47.mailsendingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSendingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
